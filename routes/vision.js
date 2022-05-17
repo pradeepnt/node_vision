11bc7a18d83ad01e1afdeee3703c7ee6bf444465
Pradeep N <30828350+pradeepnt@users.noreply.github.com>
2022-05-17 14:57:04 +0530

    Update vision.js

--- routes/vision.js
+++ routes/vision.js
@@ -1,12 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var AWS = require('aws-sdk');
 
 router.post('/classify', function(req, res, next) {
   // DON'T return the hardcoded response after implementing the backend
   let response = ["shoe", "red", "nike"];
 
   // Your code starts here //
+  const config = new AWS.Config({
+    accessKeyId:"AKIARAR74F5B2ZJFROOU",
+    secretAccessKey:"58t6FYfBVhi0FhEKFwxOWExsgASY3dtg6EHAPcVP",
+    region:"us-east-1"
+  })
 
+  const client = new AWS.Rekognition();
+  const params = {
+    Image: {
+      S3Object:{
+        Bucket: bucket,
+        Name: photo
+      },
+    },
+    MaxLabels:10
+  }
+
+  if (err) {
+    console.log(err, err.stack);
+  } else {
+    console.log('Detect labels: ${photo}');
+    res.labels;
+  }
   // Your code ends here //
 
   res.json({
